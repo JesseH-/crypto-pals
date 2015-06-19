70857703980e34964a8206e52aa6d02c6d47d8bb
Jesse Huard <jhuard@ualberta.ca>
2015-06-18 19:44:23 -0600

    CHALLENGE: Modify set 1 challenge 3 solution to use new best-fit function.

--- src/bin/set1/c3.rs
+++ src/bin/set1/c3.rs
@@ -2,11 +2,8 @@ extern crate crypto;
 extern crate rustc_serialize;
 
 use std::io;
-use std::cmp::Ordering::{Equal};
     
-use crypto::crypto::freq_scoring::{score_freq};
-use crypto::util::{hex_string_xor};
-use rustc_serialize::hex::{FromHex, ToHex};
+use crypto::crypto::freq_scoring::{get_best_fit};
 
 fn main() {
     let mut hex = String::new();
@@ -15,18 +12,7 @@ fn main() {
         .ok()
         .expect("Failed to read hex input");
 
-    let mut xors = Vec::new();
-    for i in 0..128 {
-        let u = i as u8;
-        let rep = (0..hex.len()).map(|_| u).collect::<Vec<u8>>().to_hex();
-        let bytes = hex_string_xor(&hex, &rep).from_hex().unwrap();
-        let result = String::from_utf8(bytes).unwrap();
-        xors.push((i, result));
-    }
-
-    xors.sort_by(|&(i, ref x), &(j, ref y)|
-                 (&score_freq(&y)).partial_cmp(&score_freq(&x))
-                 .unwrap_or(Equal));
-    println!("Decoded: {}\nScore: {}", xors[0].1, score_freq(&xors[0].1));
+    let result = get_best_fit(&hex);
+    println!("Decoded: {}\nScore: {}", result.decoded, result.score);
 }
 
