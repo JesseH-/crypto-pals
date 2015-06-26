28c99537749c995c567d2db5f41991357ef2397e
Jesse Huard <jhuard@ualberta.ca>
2015-06-26 00:01:49 -0600

    CRYPTO: Add function for aes encryption in cbc mode.

--- src/crypto/encrypt.rs
+++ src/crypto/encrypt.rs
@@ -1,9 +1,13 @@
 extern crate crypto;
 extern crate rand;
 
+use std::cmp::{min};
+
 use self::crypto::{symmetriccipher, buffer, aes, blockmodes};
 use self::crypto::buffer::{ReadBuffer, WriteBuffer, BufferResult};
 use self::rand::{thread_rng, Rng};
+
+use util::{fixed_xor};
 
 pub fn generate_key() -> Vec<u8> {
     let mut v = vec![0u8; 16];
@@ -35,6 +39,31 @@ pub fn encrypt_aes_ecb(encrypted: &[u8], key: &[u8]) ->
     }
 
     Ok(final_result)
+}
+
+pub fn encrypt_aes_cbc(plaintext: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8>{
+    assert!(key.len() == iv.len());
+    let block_size = key.len();
+    let mut final_result = Vec::<u8>::new();
+
+    let mut carry = iv.to_vec();
+    for i in 0 .. (plaintext.len() + block_size -1) / block_size {
+        let end = min((i + 1) * block_size, plaintext.len());
+        let next_block = &plaintext[i * block_size .. end];
+
+        let mut block;
+        if next_block.len() < block_size {
+            block = next_block.to_vec();
+            pkcs_pad(&mut block, block_size);
+            block = fixed_xor(&block, &carry);
+        } else {
+            block = fixed_xor(next_block, &carry);
+        }
+
+        carry = encrypt_aes_ecb(&block, key).unwrap();
+        final_result.extend(carry.iter().cloned());
+    }
+    final_result
 }
 
 pub fn pkcs_pad(blocks: &mut Vec<u8>, length: usize) {
