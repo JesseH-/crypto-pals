a255fe992d68fe9a58014a4e5aba9be96731557a
Jesse Huard <jhuard@ualberta.ca>
2015-06-25 22:34:55 -0600

    CHALLENGE: Decrypt file in set 2 challenge 10.

---
+++ src/bin/set2/c10.rs
@@ -0,0 +1,21 @@
+extern crate crypto;
+extern crate cryptopals;
+
+use std::io;
+use std::io::Read;
+
+use cryptopals::crypto::decrypt::{decrypt_aes_cbc};
+use cryptopals::util::{base64_string_to_bytes};
+
+fn main() {
+    let mut message = String::new();
+    io::stdin().read_to_string(&mut message)
+        .ok()
+        .expect("Failed to read message content.");
+    let hex = base64_string_to_bytes(&message);
+    let key = "YELLOW SUBMARINE".as_bytes();
+    let iv = vec![0u8; 16];
+    let decrypted_bytes = decrypt_aes_cbc(&hex, &key, &iv);
+    let decrypted = String::from_utf8(decrypted_bytes).ok().unwrap();
+    println!("{}", decrypted);
+}
