449b3c23cb51ac500a90beb1f4db17ad58412c54
Jesse Huard <jhuard@ualberta.ca>
2015-06-29 21:13:13 -0600

    CRYPTO: Add enum to distinguish between ECB and CBC encryption.

--- src/crypto/encrypt.rs
+++ src/crypto/encrypt.rs
@@ -9,6 +9,12 @@ use self::rand::{thread_rng, Rng};
 
 use util::{fixed_xor};
 
+#[derive(PartialEq)]
+pub enum Mode {
+    ECB,
+    CBC,
+}
+
 pub fn generate_key() -> Vec<u8> {
     let mut v = vec![0u8; 16];
     thread_rng().fill_bytes(&mut v);
