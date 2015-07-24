f7d2e8a65b7386e21ea57d0b65a120309aa0749c
Jesse Huard <jhuard@ualberta.ca>
2015-07-23 22:12:55 -0600

    UTIL: Add function for encoding cookies.

--- src/util/cookie.rs
+++ src/util/cookie.rs
@@ -6,3 +6,15 @@ pub fn parse_cookie(cookie: &str) -> HashMap<&str, &str> {
         .collect();
     kvs.iter().map(|vec| (vec[0], vec[1])).collect()
 }
+
+pub fn encode_cookie(map: &HashMap<&str, &str>) -> String {
+    let mut result = String::new();
+    for (&k, &v) in map.iter() {
+        result.push_str(k);
+        result.push_str("=");
+        result.push_str(v);
+        result.push_str("&");
+    }
+    result.pop();
+    result
+}
