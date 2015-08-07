fff09c2ad147d7afef6d91b8a6c021c3648ea32e
Jesse Huard <jhuard@ualberta.ca>
2015-08-06 18:10:09 -0600

    CHALLENGE: Add test to solve Set 3 Challenge 23.

--- tests/test_rng.rs
+++ tests/test_rng.rs
@@ -38,3 +38,16 @@ fn test_untemper() {
         assert_eq!(untemper(output), m.state[i]);
     }
 }
+
+#[test]
+fn test_rng_clone_from_output() {
+    let mut m: MT = SeedableRng::from_seed(314159);
+    let mut state = [0; 624];
+    for i in 0 .. 624 {
+        state[i] = untemper(m.gen::<u32>());
+    }
+    let mut cloned = MT { state: state, index: 624 };
+    for _ in 0 .. 1024 {
+        assert_eq!(cloned.gen::<u32>(), m.gen::<u32>());
+    }
+}
